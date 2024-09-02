040452fc6c8c265e43264f5e3d3a94ef7395563d
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-09-02 11:45:36 +0500

    add js code for score

--- script.js
+++ script.js
@@ -71,11 +71,19 @@ function selectAnswer(e){
     const selectedBtn= e.target;
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
-        selectedBtn.classList.add("correct")
+        selectedBtn.classList.add("correct");
+        score++;
 
     }else{
         selectedBtn.classList.add("incorrect")
     }
+    Array.from(answerButtons.children).forEach(button =>{
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled=true;
+    });
+    nextButton.style.display='block'
 }
 
 startQuiz();
