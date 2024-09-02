bd704d3b33470cfdfa4b2f621b344dd2f6676646
sajjadahmad30 <sajjadahmad5578@gmail.com>
2024-09-02 10:48:10 +0500

    add javaScript object that contains multiple questions

---
+++ script.js
@@ -0,0 +1,29 @@
+const questions=[
+    {
+        question:"what is the capital of pakistan?",
+        answers:[
+        {text:"Peshawer", correct:false},
+        {text:"Lahore", correct:false},
+        {text:"Karachi", correct:false},
+        {text:"Islamabad", correct:true},
+        ]
+    },
+    {
+        question:"what is the old college in pakistan?",
+        answers:[
+        {text:"Islamia college peshawer", correct:true},
+        {text:"Edward college pheshawar", correct:false},
+        {text:"GC lahore", correct:false},
+        {text:"Panjab college", correct:false},
+        ]
+    },
+    {
+        question:"what is the PM of pakistan?",
+        answers:[
+            {text:"Imran khan", correct:false},
+            {text:"Zardari", correct:false},
+            {text:"Shehbaz sharif", correct:true},
+        {text:"Maulana fazlur rahman", correct:false},
+        ]
+    },
+];
